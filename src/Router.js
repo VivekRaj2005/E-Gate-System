849c30463716d3e5e83520b1a6e428f282ea5e87
Vivek Raj <70755936+VivekRaj2005@users.noreply.github.com>
2024-04-12 23:13:35 +0530

    Added Tools

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
+import React from 'react';
 
 function Router() {
   return (
